89496bc4fe53a3c0b5af7a729b3102485a46b739
JULO01 <90795588+JULO01@users.noreply.github.com>
2022-04-01 20:55:38 +0200

    feat: add NameInput component and vuex store data
    NameInput Component adds the input to vuex-store-state on Button click.

---
+++ src/components/NameInput.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <v-row>
+      <v-text-field v-model="guestName" label="Guest name"></v-text-field>
+      <NameInputButton @button-pressed="addGuest(guestName), guestName=''" />
+    </v-row>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import {mapMutations} from "vuex";
+import NameInputButton from "@/components/NameInputButton.vue";
+
+export default Vue.extend({
+  name: "NameInput",
+  components: {
+    NameInputButton,
+  },
+  data() {
+    return {
+      guestName: new String(),
+    };
+  },
+  methods: {
+      ...mapMutations(["addGuest"]),
+  },
+  mounted(){
+      this.guestName = "";
+  }
+});
+</script>
--- src/store/index.ts
+++ src/store/index.ts
@@ -5,8 +5,12 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    guests: new Array(),
   },
   mutations: {
+    addGuest(state, guest){
+      state.guests.push(guest);
+    }
   },
   actions: {
   },
