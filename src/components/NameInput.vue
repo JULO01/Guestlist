c74a7cc78e0412f58da664652c2b62f119debbc9
JULO01 <90795588+JULO01@users.noreply.github.com>
2022-04-03 12:43:55 +0200

    refactor: outsourced the @click of the button into a function

--- src/components/NameInput.vue
+++ src/components/NameInput.vue
@@ -1,15 +1,23 @@
 <template>
   <div>
     <v-row>
-      <v-text-field v-model="guestName" label="Guest name"></v-text-field>
-      <NameInputButton @button-pressed="addGuest(guestName), guestName=''" />
+      <v-text-field
+        @keydown.enter="addGuestClicked(guestName)"
+        v-model="guestName"
+        label="Guest name"
+      >
+      </v-text-field>
+      <NameInputButton
+        class="input-button"
+        @button-pressed="addGuestClicked(guestName)"
+      />
     </v-row>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import {mapMutations} from "vuex";
+import { mapMutations } from "vuex";
 import NameInputButton from "@/components/NameInputButton.vue";
 
 export default Vue.extend({
@@ -23,10 +31,15 @@ export default Vue.extend({
     };
   },
   methods: {
-      ...mapMutations(["addGuest"]),
-  },
-  mounted(){
+    ...mapMutations(["addGuest"]),
+    addGuestClicked(guest: String) {
+      if (guest == "") return;
+      this.addGuest(guest);
       this.guestName = "";
-  }
+    },
+  },
+  mounted() {
+    this.guestName = "";
+  },
 });
 </script>
